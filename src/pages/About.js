90dc5fe97db7ffcc2efb2f749b88485f77fd2e22
Ruslan <rus@gmx.com>
2021-06-20 17:22:35 +0100

    Add paragraph

--- src/pages/About.js
+++ src/pages/About.js
@@ -4,6 +4,14 @@ const About = () => {
   return (
     <section className="section about-section">
       <h1 className="section-title">About us</h1>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore,
+        voluptates? Qui iure facilis possimus! Velit cupiditate libero facilis
+        exercitationem cum numquam veniam sunt excepturi neque molestias a amet,
+        optio, possimus voluptatem officiis quisquam, sit adipisci doloremque
+        architecto fugit consectetur maxime. Esse ullam impedit accusantium
+        molestiae veniam, aspernatur quibusdam ut! Voluptatum!
+      </p>
     </section>
   );
 };
