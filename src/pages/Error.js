282ec56accbe39ce4b4954423dd668e31e27fae9
Ruslan <rus@gmx.com>
2021-06-20 17:24:07 +0100

    Add error-page section

--- src/pages/Error.js
+++ src/pages/Error.js
@@ -2,7 +2,11 @@ import React from 'react';
 import { Link } from 'react-router-dom';
 
 const Error = () => {
-  return <h2>error page</h2>;
+  return (
+    <section className="error-page section">
+      <h2>error page</h2>{' '}
+    </section>
+  );
 };
 
 export default Error;
