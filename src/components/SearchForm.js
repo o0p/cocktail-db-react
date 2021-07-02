50162c601bc495a7d30bd50bdc1cbb2de7dbe569
Ruslan <rus@gmx.com>
2021-07-02 23:47:58 +0100

    Add search-form

--- src/components/SearchForm.js
+++ src/components/SearchForm.js
@@ -4,7 +4,11 @@ import { useGlobalContext } from '../context';
 const SearchForm = () => {
   const { setSearchTearm } = useGlobalContext();
   const searchValue = React.useRef('');
-  return <section className="section search"></section>;
+  return (
+    <section className="section search">
+      <form action="" className="search-form"></form>
+    </section>
+  );
 };
 
 export default SearchForm;
